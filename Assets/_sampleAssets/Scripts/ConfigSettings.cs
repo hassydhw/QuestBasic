8aea9402e009916acd4b3ebf28ac67e8d90ff58c
hassydh <g051tg2017@dhw.ac.jp>
2020-05-22 18:41:21 +0900

    feat add version number text for every scene

---
+++ Assets/_sampleAssets/Scripts/ConfigSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigSettings : MonoBehaviour
+{
+
+    //バージョン　１＋OculusIntegrationの対応バージョン　＋　0から始めるマイナーバージョン
+    //中身大きく変えたら2にあげる
+    public static string versionNumber = "1.16.0";//20200522　バージョン管理開始
+
+}
