d36e19a18a86834a35b416ab51fbc2c4f4cb529f
hassyHyper <mxk02371@gmail.com>
2021-04-01 15:54:18 +0900

    tentative commit for handtracking test

---
+++ Assets/HandTrackingSample.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandTrackingSample : MonoBehaviour
+{
+    [SerializeField] TextMesh valueText;
+
+    [SerializeField] OVRHand ovrHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        valueText.text = ovrHand.GetFingerPinchStrength(OVRHand.HandFinger.Thumb).ToString("F2");
+    }
+}
--- Assets/_QuestBasics/sampleAssets/Scripts/ControllerInput.cs
+++ Assets/_QuestBasics/sampleAssets/Scripts/ControllerInput.cs
@@ -2,7 +2,6 @@
 
 public class ControllerInput : MonoBehaviour
 {
-
     public TextMesh buttonTouchText;
     public TextMesh buttonDownText;
     public TextMesh stickValueText;
@@ -11,7 +10,6 @@ public class ControllerInput : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
     }
 
     // Update is called once per frame
@@ -27,9 +25,8 @@ public class ControllerInput : MonoBehaviour
     private void CheckStickValue()
     {
         stickValueText.text = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick).ToString()
-        + "\n"
-        + OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).ToString();
-
+                              + "\n"
+                              + OVRInput.Get(OVRInput.RawAxis2D.RThumbstick).ToString();
     }
 
     private void CheckButtonDown()
@@ -39,36 +36,43 @@ public class ControllerInput : MonoBehaviour
             //Aボタンを押した際の処理
             buttonDownText.text = "A pressed";
         }
+
         if (OVRInput.GetDown(OVRInput.RawButton.B))
         {
             //Bボタンを押した際の処理
             buttonDownText.text = "B pressed";
         }
+
         if (OVRInput.GetDown(OVRInput.RawButton.X))
         {
             //Xボタンを押した際の処理
             buttonDownText.text = "X pressed";
         }
+
         if (OVRInput.GetDown(OVRInput.RawButton.Y))
         {
             //Yボタンを押した際の処理
             buttonDownText.text = "Y pressed";
         }
+
         if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
         {
             //右人差し指トリガーを押した際の処理
             buttonDownText.text = "R_index pressed";
         }
+
         if (OVRInput.GetDown(OVRInput.RawButton.RHandTrigger))
         {
             //右中指トリガーを押した際の処理
             buttonDownText.text = "R_hand_trigger pressed";
         }
+
         if (OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger))
         {
             //左人差し指トリガーを押した際の処理
             buttonDownText.text = "L_index pressed";
         }
+
         if (OVRInput.GetDown(OVRInput.RawButton.LHandTrigger))
         {
             //左中指トリガーを押した際の処理
@@ -80,11 +84,13 @@ public class ControllerInput : MonoBehaviour
             //左スティックを押した際の処理
             buttonDownText.text = "L_StickButton pressed";
         }
+
         if (OVRInput.GetDown(OVRInput.RawButton.RThumbstick))
         {
             //右スティックを押した際の処理
             buttonDownText.text = "R_StickButton pressed";
         }
+
         if (OVRInput.GetDown(OVRInput.RawButton.Start))
         {
             //左のスタートボタン
@@ -99,16 +105,19 @@ public class ControllerInput : MonoBehaviour
             //Aボタンをタッチした際の処理         
             buttonTouchText.text = "A touching";
         }
+
         if (OVRInput.Get(OVRInput.RawTouch.B))
         {
             //Bボタンをタッチした際の処理         
             buttonTouchText.text = "B touching";
         }
+
         if (OVRInput.Get(OVRInput.RawTouch.X))
         {
             //Xボタンをタッチした際の処理         
             buttonTouchText.text = "X touching";
         }
+
         if (OVRInput.Get(OVRInput.RawTouch.Y))
         {
             //Yボタンをタッチした際の処理         
@@ -119,6 +128,7 @@ public class ControllerInput : MonoBehaviour
         {
             buttonTouchText.text = "L_index touching";
         }
+
         if (OVRInput.Get(OVRInput.RawTouch.LThumbstick))
         {
             buttonTouchText.text = "L_Stick touching";
@@ -128,6 +138,7 @@ public class ControllerInput : MonoBehaviour
         {
             buttonTouchText.text = "R_index touching";
         }
+
         if (OVRInput.Get(OVRInput.RawTouch.RThumbstick))
         {
             buttonTouchText.text = "R_Stick touching";
