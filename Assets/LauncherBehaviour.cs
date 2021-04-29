e79fcf90a22fd41543cdd79e9fa0979affb8d73c
hassy_mxk <mxk02371@gmail.com>
2021-04-30 05:43:05 +0900

    feat test scdne transfer

---
+++ Assets/LauncherBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LauncherBehaviour : MonoBehaviour
+{
+    [SerializeField] private Scene [] _scenes;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSceneChangeButtonPressed()
+    {
+        SceneManager.LoadSceneAsync(_scenes[0].handle);
+    }
+    
+    
+    
+}
--- Assets/_QuestBasics/sampleAssets/Scripts/QB_ConfigSettings.cs
+++ Assets/_QuestBasics/sampleAssets/Scripts/QB_ConfigSettings.cs
@@ -10,6 +10,7 @@ public class QB_ConfigSettings : MonoBehaviour
     // public static string versionNumber = "1.16.0";//20200522　バージョン管理開始
     // public static string versionNumber = "1.18.0"; //20200522　SDK18.0対応、EventSystemのPrefab化
     // public static string versionNumber = "1.19.0"; //20200831　SDK19.1対応
-//    public static string versionNumber = "1.20.1"; //20201107　世田谷向けにアップデート、SDK20.1対応
-    public static string versionNumber = "1.25.0"; //20210401 新年度に向けてSDK更新　＆　アップデート
+    //    public static string versionNumber = "1.20.1"; //20201107　世田谷向けにアップデート、SDK20.1対応
+    //    public static string versionNumber = "1.25.0"; //20210401 新年度に向けてSDK更新　＆　アップデート
+    public static string versionNumber = "1.28.0"; //20210430 対応SDK更新、ランチャー機能追加
 }
