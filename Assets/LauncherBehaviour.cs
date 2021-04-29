8dafc8420787ec7b69efcd8b6eb60398e54b39cb
hassy_mxk <mxk02371@gmail.com>
2021-04-30 06:07:56 +0900

    feat getting scene name

--- Assets/LauncherBehaviour.cs
+++ Assets/LauncherBehaviour.cs
@@ -12,7 +12,17 @@ public class LauncherBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < _scenes.Length; i++)
+        {
+            string pathToScene = SceneUtility.GetScenePathByBuildIndex(i+1);
+            string sceneName = System.IO.Path.GetFileNameWithoutExtension(pathToScene);
+            Debug.Log(sceneName);            
+        }
         
+        // foreach (var eachScene in _scenes)
+        // {
+        //     Debug.Log(eachScene.buildIndex);
+        // }
     }
 
     // Update is called once per frame
@@ -23,7 +33,7 @@ public class LauncherBehaviour : MonoBehaviour
 
     public void OnSceneChangeButtonPressed()
     {
-        SceneManager.LoadSceneAsync(_scenes[0].handle);
+        SceneManager.LoadSceneAsync(_scenes[10].handle);
     }
     
     
